c8247a6e2a08cba4f8a70373964796387a38ddac
sansma123g123456 <128606818+sansma123g123456@users.noreply.github.com>
2026-02-19 19:02:40 +0700

    tao bot discord

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once('ready', () => {
+  console.log(`Bot đã online: ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('pong 🏓');
+  }
+});
+
+client.login('TOKEN_BOT_CUA_BAN');
